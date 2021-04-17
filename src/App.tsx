7e77088a90714956217b8b3ac5b11ca3aacf3952
Totic Branislav <branislav.totic@gmail.com>
2021-04-18 01:26:06 +0200

    Add text editor and create setup for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React from "react";
-import "./App.css";
+import { AppContainer } from "./App.css";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import { Flex, Heading, Wrapper } from "./style";
 
-const App = () => {
+const testCode: string = `
+<Wrapper direction="column" align="center">
+  <Heading color="success" as="h2" fontSize="xlg" mb="3">Task 1</Heading>
+  <Flex gap="20px" color="white">
+    <Flex bg="primary" padding="10px" borderRadius="4px">John</Flex>
+    <Flex bg="danger" padding="10px" borderRadius="4px">Don</Flex>
+    <Flex bg="warning" padding="10px" borderRadius="4px">Marry</Flex>
+  </Flex>
+</Wrapper>
+`;
+
+const editorStyle: { [key: string]: string } = {
+  border: `1px solid white`,
+  borderRadius: `4px`,
+  marginBottom: `20px`,
+};
+
+const App : React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>App.</p>
-      </header>
-    </div>
+    <AppContainer>
+      <Wrapper h="100vh" bgColor="black" direction="column" align="center" justify="center">
+        <Heading color="white" mb="3">Example:</Heading>
+          <LiveProvider code={testCode} scope={{ Flex, Heading, Wrapper }}>
+            <LiveEditor style={editorStyle} />
+            <LiveError />
+            <LivePreview />
+          </LiveProvider>
+      </Wrapper>
+    </AppContainer>
   );
 };
 
