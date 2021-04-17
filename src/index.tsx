3cf97aed893f0edc637a6402ea7733c5329c8148
Totic Branislav <branislav.totic@gmail.com>
2021-04-18 01:19:59 +0200

    Remove unused css and change it to styled

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Reset } from "styled-reset";
+import { GlobalStyle } from "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Reset />
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
